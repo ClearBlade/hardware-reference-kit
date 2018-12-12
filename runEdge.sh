527e223d272f3703cc407dc387a8f215520aca7f
JNguyen93 <justinmnguyen@hotmail.com>
2018-12-12 13:49:59 -0600

    Fixed runEdge.sh

--- runEdge.sh
+++ runEdge.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 
-./edge -db=sqlite -sqlite-path=./edge.db -sqlite-path-users=./edgeusers.db -edge-id="RobsMBP" -edge-ip="localhost" -novi-ip="localhost" -edge-cookie="FakeToken" -insecure=true -platform-port="1883" -parent-system="freddy" -log-level=debug -provisioning-mode=true -provisioning-system=a089a6a80bc2add5fbd6b3e7b08101
+./edge -db=sqlite -sqlite-path=./edge.db -sqlite-path-users=./edgeusers.db -edge-id="unprovisioned" -edge-ip="localhost" -novi-ip="localhost" -edge-cookie="null" -platform-port="1883" -parent-system="null" -log-level=debug -provisioning-mode=true -provisioning-system=a089a6a80bc2add5fbd6b3e7b08101
