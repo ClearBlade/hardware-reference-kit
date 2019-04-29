9349412ae979c6f396439fefeb07032cf0861310
Clark Bynum <cbynum@clearblade.com>
2019-04-29 12:02:28 -0500

    migrated to cb-dev-kit

---
+++ ClearBladeDeploymentKit/cb-dev-kit/flagConsts.js
@@ -0,0 +1,16 @@
+module.exports = {
+  portal: 'portal',
+  widgetId: 'widgetId',
+  internalResource: 'internalResource',
+  service: 'service',
+  library: 'library',
+  type: 'type',
+  messagePort: 'messagePort',
+  noSSL: 'noSSL',
+  caPath: 'caPath',
+  all: 'all',
+  allServices: 'all-services',
+  allLibraries: 'all-libraries',
+  allPortals: 'all-portals',
+  allWidgets: 'all-widgets'
+}
