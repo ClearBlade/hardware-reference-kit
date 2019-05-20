026a65f6abc666bea28cbdb6d55fc862a97efa58
Clark Bynum <cbynum@clearblade.com>
2019-05-20 17:35:05 -0500

    cypress test for preconfigured platform

--- cypress/integration/cdk.js
+++ cypress/integration/cdk.js
@@ -51,6 +51,65 @@ describe("preconfigured platform and developer", () => {
   });
 });
 
+describe("preconfigured platform", () => {
+  beforeEach(() => {
+    cy.server();
+    cy.visit(CDK_PORTAL_URL);
+    cy.fixture("preconfiguredFlows/platform").as("fxWorkflow");
+    cy.route("POST", "**/RetrieveWorkflowConfig", "@fxWorkflow").as(
+      "preconfiguredPlatform"
+    );
+
+    cy.wait("@preconfiguredPlatform");
+    cy.getCy("begin").click();
+  });
+
+  it("should not need to set up platform info", () => {
+    cy.getCy("dev-email").type("a@cypress.com");
+    cy.getCy("dev-password").type("cypress{enter}");
+    cy.getCy("system-template-id").select(
+      "rreinold_anomaly-detection-template"
+    );
+    cy.getCy("continue").click();
+    cy.getCy("edge-options").within(() => {
+      cy.getCy("option-NEW").click();
+    });
+    cy.getCy("edge-id").type("cypressEdge{enter}");
+    cy.route({
+      method: "POST",
+      url: "**/SetupPlatformSystemForEdge",
+      response: { results: "hello results!!" }
+    }).as("SetupPlatformSystemForEdge");
+    cy.getCy("retarget-btn").click();
+    cy.wait("@SetupPlatformSystemForEdge").then(req => {
+      cy.wrap(req.request.body.PLATFORM)
+        .its("flow")
+        .should("eq", "PRECONFIGURED");
+      cy.wrap(req.request.body.DEVELOPER)
+        .its("flow")
+        .should("eq", "EXISTING");
+      cy.wrap(req.request.body.DEVELOPER)
+        .its("devEmail")
+        .should("eq", "a@cypress.com");
+      cy.wrap(req.request.body.DEVELOPER)
+        .its("devPassword")
+        .should("eq", "cypress");
+      cy.wrap(req.request.body.SYSTEM)
+        .its("flow")
+        .should("eq", "IPM");
+      cy.wrap(req.request.body.SYSTEM)
+        .its("repoName")
+        .should("eq", "anomaly-detection-template");
+      cy.wrap(req.request.body.EDGE)
+        .its("flow")
+        .should("eq", "NEW");
+      cy.wrap(req.request.body.EDGE)
+        .its("edgeID")
+        .should("eq", "cypressEdge");
+    });
+  });
+});
+
 //   it("existing platform", () => {});
 
 //   it("new developer", () => {});
